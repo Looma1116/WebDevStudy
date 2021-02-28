0da822e03456da9aaf567ce1c8166356afad964b
Looma <41144011+Looma1116@users.noreply.github.com>
2021-02-28 23:28:08 +0900

    Add files via upload

---
+++ colors.js
@@ -0,0 +1,32 @@
+  var Body={
+    setBackColor:function(color){
+      document.querySelector('body').style.backgroundColor=color;
+    },
+    setColor:function(color){
+      document.querySelector('body').style.color=color;
+    }
+  };
+
+  var Link={
+    setColor:function(color){
+      var link = document.querySelectorAll('a');
+      for ( var i = 0; i < link.length; i++){
+        link[i].style.color = color;
+      }
+    }
+  };
+
+  function nightMode(self){
+    if(self.value === 'night'){
+      Body.setBackColor('#2E2E2E');
+      Body.setColor('white');
+      self.value = 'day';
+      Link.setColor('#BCA9F5');
+    }
+    else{
+      Body.setBackColor('white');
+      Body.setColor('black');
+      self.value = 'night';
+      Link.setColor('blue');
+    }
+  }
