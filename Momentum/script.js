2d45d395271325118146c9663dd748d99ef112c5
Looma1116 <jksjsmlove2@naver.com>
2021-09-10 12:38:32 +0900

    updated

---
+++ Momentum/script.js
@@ -0,0 +1,11 @@
+const player = {
+    Name: 'Joon',
+    points: 10,
+    fat: true,
+};
+
+console.log(player);
+console.log(player.Name);
+console.log(player["Name"]);
+player.lastName = "potato";
+console.log(player);
