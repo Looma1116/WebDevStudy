5cd44c1956963d7e25923cb8dda5af37bd0464d8
Looma1116 <jksjsmlove2@naver.com>
2021-09-14 00:32:02 +0900

    yeah

--- Momentum/lecture.js
+++ Momentum/lecture.js
@@ -15,15 +15,57 @@ const text1 = document.getElementById('tomato');
 const array = document.getElementsByClassName('food');
 
 //css selector형태로 읽어올 수 있는 querySelector .div h1 같은 형태
-const title = document.querySelector(".hello h1");
+conext1 = document.querySelector(".hello h1");
 //근데 조건에 맞는 첫 번째 element만 가져온다.
-const title = document.querySelector("#tomato");
+const text1 = document.querySelector("#tomato");
 
 //다 가져오고 싶으면?
-const title = document.querySelectorAll(".div");
+const text1 = document.querySelectorAll(".div");
 
 // element event
-function handleTitleClick() {
-    console.log("title was clicked!");
+function handleText1Click() {
+    if (flag == true){
+        text1.style.color = "blue";
+        flag = false;
+    }
+    else {
+        text1.style.color = 'black';
+        flag = true;
+    }
+
 }
-title.addEventListener("click", handleTitleClick);
+function handleMouseEnter() {
+    text1.innerText = "Mouse is here";
+}
+
+function handleMouseLeave() {
+    text1.innerText = "Mouse is Gone!";
+}
+text1.addEventListener("click", handleText1Click);
+text1.addEventListener("mouseenter", handleMouseEnter);
+text1.addEventListener("mouseleave", handleMouseLeave);
+//이렇게도 됨
+text1.onclick = handleTitleClick;
+text1.onmouseenter = handleMouseEnter;
+text1.onmouseleave = handleMouseLeave;
+
+//window events
+function handleWindowResize(){
+    document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy() {
+    alert("copier!");
+}
+
+function handleWindowOffline() {
+    alert("SOS no Internet");
+}
+
+function handleWindowOnline() {
+    alert("Internet ON!");
+}
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
--- Momentum/script.js
+++ Momentum/script.js
@@ -1,15 +1,47 @@
-const title = document.querySelector("div.hello h1");
+const text1 = document.querySelector("div.hello h1");
 
 let flag = true;
-function handleTitleClick() {
+function handleText1Click() {
     if (flag == true){
-        title.style.color = "blue";
+        text1.style.color = "blue";
         flag = false;
     }
     else {
-        title.style.color = 'black';
+        text1.style.color = 'black';
         flag = true;
     }
-
 }
-title.addEventListener("click", handleTitleClick);
+
+function handleMouseEnter() {
+    text1.innerText = "Mouse is here";
+}
+
+function handleMouseLeave() {
+    text1.innerText = "Mouse is Gone!";
+}
+text1.addEventListener("click", handleText1Click);
+text1.addEventListener("mouseenter", handleMouseEnter);
+text1.addEventListener("mouseleave", handleMouseLeave);
+
+text1.onmouseenter = handleMouseEnter;
+text1.onmouseleave = handleMouseLeave;
+
+function handleWindowResize(){
+    document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy() {
+    alert("copier!");
+}
+
+function handleWindowOffline() {
+    alert("SOS no Internet");
+}
+
+function handleWindowOnline() {
+    alert("Internet ON!");
+}
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
