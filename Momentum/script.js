cd9c3f0d78a67fb08d53edcbf4da9b04d2caffb0
Looma1116 <jksjsmlove2@naver.com>
2021-09-14 20:41:33 +0900

    cssfunction

--- Momentum/script.js
+++ Momentum/script.js
@@ -1,47 +1,7 @@
 const text1 = document.querySelector("div.hello h1");
 
-let flag = true;
 function handleText1Click() {
-    if (flag == true){
-        text1.style.color = "blue";
-        flag = false;
-    }
-    else {
-        text1.style.color = 'black';
-        flag = true;
-    }
+    text1.classList.toggle("active");
 }
 
-function handleMouseEnter() {
-    text1.innerText = "Mouse is here";
-}
-
-function handleMouseLeave() {
-    text1.innerText = "Mouse is Gone!";
-}
 text1.addEventListener("click", handleText1Click);
-text1.addEventListener("mouseenter", handleMouseEnter);
-text1.addEventListener("mouseleave", handleMouseLeave);
-
-text1.onmouseenter = handleMouseEnter;
-text1.onmouseleave = handleMouseLeave;
-
-function handleWindowResize(){
-    document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy() {
-    alert("copier!");
-}
-
-function handleWindowOffline() {
-    alert("SOS no Internet");
-}
-
-function handleWindowOnline() {
-    alert("Internet ON!");
-}
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
